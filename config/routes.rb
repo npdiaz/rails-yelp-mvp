5260f94c7620b7efa38056180eb6eaa811ce02d8
Nerea Pascual Diaz <nereapasc@gmail.com>
2019-02-27 15:31:04 +0100

    routes, controllers and models done, view undefined

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  # read all
+  # get 'restaurants', to: 'restaurants#index'
+  # # read one
+  # get 'restaurants/:id', to: 'restaurants#show', as: 'show'
+  # # add new restaurant
+  # get 'restaurants/new', to: 'restaurants#new'
+  # post 'restaurants', to: 'restaurants#create', as: 'create'
+  # add new review to restaurant
+    # get 'restaurants/:id/reviews/new', to: 'revies#new'
+    # post 'restaurants/:id/reviews', to: 'reviews#create'
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...' Restaurant.destroy_all
+puts 'Creating restaurants...'
+
+Restaurant.new(name: 'Itsu', address: 'Well St 39', phone_number: '232342345', category: 'japanese')
+Restaurant.new(name: 'Salsa', address: 'Paris st', phone_number: '232342345', category: 'french')
+Restaurant.new(name: 'Pizzeria', address: 'Venice street 23', phone_number: '232342345', category: 'italian')
+Restaurant.new(name: 'Maison Antoine', address: 'Rue Gray 4', phone_number: '232342345', category: 'belgian')
+Restaurant.new(name: 'Golden House', address: 'House of wonder 5', phone_number: '232342345', category: 'chinese')
